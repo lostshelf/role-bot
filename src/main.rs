7fade8935645e2cafd3515b05e856312c1f6231d
lostshelf <hadifaraz52@protonmail.com>
2022-06-14 15:27:47 -0600

    Added some TODOs

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,24 @@ use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
-use toml::value;
+// use toml::value; // TODO: Read prefix from configuration file
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
+    async fn message(&self, _: Context, msg: Message) {
         if msg.author.bot != true {
             return;
         }
 
         if msg.content.contains('>') {
-
+            // TODO: Implement some type of function handler
         }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected.", ready.user.name)
     }
 }
 
@@ -27,6 +31,4 @@ fn main() {
         Ok(t) => t,
         Err(e) => panic!("Couldn't get token from environment: {}", e),
     };
-
-
 }
