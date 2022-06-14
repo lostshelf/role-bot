dc3f0fe7582ba5d6857e0723bc2ae83b49946845
lostshelf <hadifaraz52@protonmail.com>
2022-06-14 14:45:42 -0600

    Started on a command handler and configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 use std::env;
 
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+use toml::value;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.author.bot != true {
+            return;
+        }
+
+        if msg.content.contains('>') {
+
+        }
+    }
+}
+
 fn main() {
-    let _token = match env::var("TOKEN") {
+    let _token = match env::var("DISCORD_TOKEN") {
         Ok(t) => t,
         Err(e) => panic!("Couldn't get token from environment: {}", e),
     };
+
+
 }
